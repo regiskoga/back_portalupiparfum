c02abec08b5d570981c625fd46b26e1b72007834
regiskoga <euclidespereira2001@gmail.com>
2026-04-27 20:40:45 -0300

    Fix: Adicionar suporte para DATABASE_URL no knexfile

--- knexfile.js
+++ knexfile.js
@@ -27,13 +27,13 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
+    connection: process.env.DATABASE_URL || {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT || 5432,
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      ssl: { rejectUnauthorized: false }
+      ssl: false
     },
     pool: {
       min: 2,
